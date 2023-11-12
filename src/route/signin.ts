5da75a3c8d69207d391c6e93285006ac133d1f35
kym8821 <louie9798@naver.com>
2023-11-12 21:50:31 +0900

    [fix] : 비밀번호가 해싱되지 않는 오류 수정

--- src/route/signin.ts
+++ src/route/signin.ts
@@ -5,16 +5,10 @@ import dotenv from 'dotenv';
 import connection from '../../config/connection';
 dotenv.config();
 
-const whiteList:string[] = [process.env.ALLOWED_DOMAIN as string];
+const allowedDomainList:string[] = [process.env.ALLOWED_DOMAIN as string];
 const corsOption = {
-  origin:(origin:string, callback:Function)=>{
-    if(whiteList.indexOf(origin)!==-1){
-      return callback(null, true);
-    }else{
-      return callback(new Error("허용되지 않은 도메인입니다."));
-    }
-  }
-}
+  origin:allowedDomainList
+};
 
 const getHashedPassword = async (password : string)=>{
   const salt:string = await bcrypt.genSalt(parseInt(process.env.SALT_ROUND as string));
@@ -30,26 +24,40 @@ route.use(express.urlencoded({extended:true}));
 // 회원가입 라우터
 route.post("/", async (req, res)=>{
   const {email, password, name, birthday, githubName, baekjoonName} = req.body;
+  // 사용자 데이터 중 누락 데이터가 있다면 status:404 반환
   if(!email || !password || !name || !birthday || !githubName || !baekjoonName){
-    return res.sendStatus(400);
+    return res.json({
+      status:false,
+      message:"누락된 정보가 있습니다"
+    });
   }
-  const hashedPassword = await getHashedPassword(password);
   const selectQuery = `SELECT * FROM user WHERE email=(?)`;
   const selectParam = [email];
   try{
     const selectResult:any[] = await connection.query(selectQuery, selectParam);
+    // email이 중복되지 않으면 현재 사용자 정보 추가
     if(selectResult[0].length==0){
+      const hashedPassword = await getHashedPassword(password);
       const insertQuery = `INSERT INTO user(email, password, name, birthday, github_name, baekjoon_name) 
         VALUES (?, ?, ?, ?, ?, ?)`;
-      const insertParam = [email, password, name, birthday, githubName, baekjoonName];
+      const insertParam = [email, hashedPassword, name, birthday, githubName, baekjoonName];
       await connection.query(insertQuery, insertParam);
-      console.log(hashedPassword);
-      return res.sendStatus(200);
+      return res.json({
+        status:true,
+        message:"회원가입 성공!"
+      });
+    }else{
+      return res.json({
+        status:false,
+        message:"이미 가입한 사용자입니다."
+      });
     }
   }catch(err){
-    console.log(err);
+    return res.json({
+      status:false,
+      message:"서버 에러"
+    });
   }
-  res.sendStatus(400);
 });
 
 export default route;
