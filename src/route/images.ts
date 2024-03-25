feaabd421755d47ec77a2d69a15abc35a6d315f8
kym8821 <louie9798@naver.com>
2024-03-25 18:43:50 +0900

    [feat] dockerfile

--- src/route/images.ts
+++ src/route/images.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import { Request, Response } from "express";
-import multer from "multer";
-import ImageS3Service from "../service/images";
-import { memoryStorageMulter } from "../config/multerConfig";
+import express from 'express';
+import { Request, Response } from 'express';
+import multer from 'multer';
+import ImageS3Service from '../service/images';
+import { memoryStorageMulter } from '../config/multerConfig';
 
 const route = express.Router();
 const imageS3Service = new ImageS3Service();
 
 //multer.single("name")은 body에서 name이라는 키를 갖는 파일을 찾아서 변환해줌
-route.post("", memoryStorageMulter.single("image"), async (req: Request, res: Response) => {
+route.post('', memoryStorageMulter.single('image'), async (req: Request, res: Response) => {
   const fileData = req.file;
   const key = req.body.key;
   if (!fileData || !key) return res.sendStatus(400);
@@ -17,7 +17,7 @@ route.post("", memoryStorageMulter.single("image"), async (req: Request, res: Re
   return res.send(result);
 });
 
-route.post("/:imageKey", async (req, res) => {
+route.post('/:imageKey', async (req, res) => {
   const key = req.params.imageKey;
   const result = await imageS3Service.deleteS3Object(key);
   if (!result) return res.sendStatus(400);
